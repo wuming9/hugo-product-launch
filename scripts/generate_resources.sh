3df10cc3890a096c1faf963e9a220db8d3e14341
Pkydrip <71935857@qq.com>
2025-05-09 14:26:00 +0800

    Updated from Publii

--- scripts/generate_resources.sh
+++
@@ -1,8 +0,0 @@
-# Remove (--gc) exiting resources and generate for GitHub pages build
-hugo --minify --gc -s exampleSite --themesDir=../.. --baseURL https://janraasch.github.io/hugo-product-launch/
-
-# Generate "production"-environment resources
-hugo --minify -s exampleSite --themesDir=../..
-
-# Generate "development"-environment resources
-hugo server -s exampleSite --themesDir=../..
